6c67f53b2f185caa4372d810e2bc9e10aeb454fd
Meelunae <git@eleuna.me>
2021-09-01 22:44:49 +0200

    Fixed issue where attachments would be scanned ignoring type; handled HTTP request exception handling; Fixed/prettified output and API calls;

--- discordbot.py
+++ discordbot.py
@@ -1,17 +1,15 @@
 import discord
-import io
 from discord.ext import commands
 from PIL import Image
-import requests
 import numpy as np
+import io
+import requests
 import yaml
 import pytesseract
 
 bot = commands.Bot(command_prefix='!')
-pic_ext = ['.jpg','.png','.jpeg']
 wordlist_file = open("wordlist.yaml")
 wordlist = yaml.load(wordlist_file, Loader=yaml.FullLoader)
-
 
 @bot.event
 async def on_ready():
@@ -28,31 +26,38 @@ async def on_message(message):
 
     if message.attachments:
         for attachment in message.attachments:
-            urls.add(attachment.url)
+            # just like embeds, attachments that are not images do not interest us so we won't add them to the URL list to scan
+            if attachment.url.lower().endswith(".jpg") or attachment.url.lower().endswith(".png") or attachment.url.lower().endswith(".jpeg"):
+                urls.add(attachment.url) 
 
     if len(urls) > 0:
         for url in urls:
             if scancontent(url):
-                print("Found CP mention, get yeeted!")
+                print("Found blacklisted word mention, get yeeted!")
                 user = message.author
-                role = discord.utils.get(message.guild.roles, name=wordlist["muted_role"])
+                role = message.guild.get_role(wordlist["punished_role_ID"])
                 channel = bot.get_channel(wordlist["alert_channel"])
                 embed = discord.Embed(title="User muted", description="", color=0x00ff00)
-                embed.add_field(name="User", value=user, inline=False)
-                embed.add_field(name="Reason", value="Suspected CSAM invite", inline=False)
+                embed.add_field(name="User", value="{} <\@{}>".format(user, user.id), inline=False)
+                embed.add_field(name="Reason", value="Blacklisted word detected in image", inline=False)
                 await channel.send(embed=embed)
                 await user.add_roles(role)
                 await message.delete()
 
 
 def scancontent(url):
-                    im = Image.open(requests.get(url, stream=True).raw)
-                    image =  np.array(im)
-                    text = pytesseract.image_to_string(image)
-                    for word in wordlist["banned_words"]:
-                        if word in text.lower():
-                            return True
-                    return False
+    try:
+        r = requests.get(url, stream=True)
+        r.raise_for_status()
+        im = Image.open(io.BytesIO(r.content))
+        image =  np.array(im)
+        text = pytesseract.image_to_string(image)
+        for word in wordlist["banned_words"]:
+            if word in text.lower():
+                return True
+    except requests.exceptions.HTTPError as err:
+        print(err)
+        return False
 
 
 bot.run('token')
